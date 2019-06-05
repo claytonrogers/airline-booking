d88fdca44ae025fe5ebce62a7869e256f721263a
Clayton Rogers <clayton.rogers@gmail.com>
2019-06-05 13:44:49 -0600

    clean up Vuex syntact w/ async/await

--- src/store/catalog/actions.js
+++ src/store/catalog/actions.js
@@ -1,6 +1,6 @@
 import Flight from "../../shared/models/FlightClass";
 import axios from "axios";
-import Amplify, { API, graphqlOperation } from "aws-amplify";
+import { API, graphqlOperation } from "aws-amplify";
 import { listFlights } from "../../graphql/queries";
 
 /**
@@ -32,12 +32,10 @@ import { listFlights } from "../../graphql/queries";
  *    this.filteredFlights = this.sortByDeparture(this.flights);
  * }
  */
-export function fetchFlights({ commit }, { date, departure, arrival }) {
-  return new Promise(async (resolve, reject) => {
+export async function fetchFlights({ commit }, { date, departure, arrival }) {
     commit("SET_LOADER", true);
     try {
-      //const { data: flightData } = await axios.get("/mocks/flights.json");
-      console.log(date);
+      // listFlights query filter
       const flightFilter = {
         filter: {
           departureDate: {
@@ -51,24 +49,24 @@ export function fetchFlights({ commit }, { date, departure, arrival }) {
           }
         }
       };
-
+      // graphQL API
       const {
-        data: { listFlights: { items: flightData } }
-      }  = await API.graphql(graphqlOperation(listFlights, flightFilter));
+        data: { 
+          listFlights: { items: flightData } 
+        } 
+      } = await API.graphql(graphqlOperation(listFlights, flightFilter));
 
-      console.log(flightData);
-
+      // data mutations happen within a Flight class
+      // here we convert graphQL results into an array of Flights
+      // before committing to the Vuex state mgmt (store)
       const flights = flightData.map(flight => new Flight(flight));
 
       commit("SET_FLIGHTS", flights);
       commit("SET_LOADER", false);
-      resolve();
     } catch (error) {
       console.error(error);
       commit("SET_LOADER", false);
-      reject(error);
     }
-  });
 }
 
 /**
